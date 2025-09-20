e9c0fe1b162ce262ccc56ef11bb06cee6cb0709c
CardiaCeres <Chilia0324@gmail.com>
2025-09-20 12:51:25 +0800

    Create AppDbContext.cs

---
+++ manufacturing_system/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+using ManufacturingSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ManufacturingSystem.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; } = null!;
+        public DbSet<Order> Orders { get; set; } = null!;
+    }
+}
