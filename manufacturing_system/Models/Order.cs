1f31dc31b00f4729c9d11e6b0670a0d6d40aed85
CardiaCeres <Chilia0324@gmail.com>
2025-09-20 12:51:44 +0800

    Create Order.cs

---
+++ manufacturing_system/Models/Order.cs
@@ -0,0 +1,17 @@
+namespace ManufacturingSystem.Models
+{
+    public class Order
+    {
+        public long Id { get; set; }
+        public string? OrderNumber { get; set; }
+        public string? CustomerName { get; set; }
+        public string? MaterialName { get; set; }
+        public string? ProductName { get; set; }
+        public int? Quantity { get; set; }
+        public double? Price { get; set; }
+        public double? TotalAmount { get; set; }
+        public string? Status { get; set; }
+        public string? OrderDate { get; set; }
+        public long UserId { get; set; }
+    }
+}
