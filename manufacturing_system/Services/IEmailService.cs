eb0e50a0b0b1a38ae9f694dee0f7521107550662
CardiaCeres <Chilia0324@gmail.com>
2025-10-10 17:22:27 +0800

    Update IEmailService.cs

--- manufacturing_system/Services/IEmailService.cs
+++ manufacturing_system/Services/IEmailService.cs
@@ -4,13 +4,13 @@ namespace ManufacturingSystem.Services
 {
     public interface IEmailService
     {
-        /// 發送重設密碼信
+        // 發送重設密碼信
         Task SendResetPasswordEmailAsync(string toEmail, string resetUrl);
 
-        /// 發送一般通知信
+        // 發送一般通知信
         Task SendNotificationEmailAsync(string toEmail, string subject, string htmlContent);
 
-        /// 發送自訂內容信件
+        // 發送自訂內容信件
         Task SendCustomEmailAsync(string toEmail, string fromEmail, string subject, string htmlContent);
     }
 }
