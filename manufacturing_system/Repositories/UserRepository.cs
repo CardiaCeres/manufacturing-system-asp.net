ccfbd47541caf0b278673e11a6049915813ddae7
CardiaCeres <Chilia0324@gmail.com>
2025-10-10 13:46:48 +0800

    Update UserRepository.cs

--- manufacturing_system/Repositories/UserRepository.cs
+++ manufacturing_system/Repositories/UserRepository.cs
@@ -1,6 +1,7 @@
 using ManufacturingSystem.Data;
 using ManufacturingSystem.Models;
 using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace ManufacturingSystem.Repositories
@@ -19,12 +20,37 @@ namespace ManufacturingSystem.Repositories
         public async Task<User?> GetByResetTokenAsync(string resetToken) =>
             await _context.Users.FirstOrDefaultAsync(u => u.ResetToken == resetToken);
 
+        public async Task<User?> GetByIdAsync(long id) =>
+            await _context.Users.FindAsync(id);
+
         public async Task<User> AddOrUpdateAsync(User user)
         {
             if (user.Id == 0) _context.Users.Add(user);
             else _context.Users.Update(user);
+
             await _context.SaveChangesAsync();
             return user;
         }
+
+        public async Task DeleteAsync(long id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if (user != null)
+            {
+                _context.Users.Remove(user);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<List<User>> GetByDepartmentAsync(string department) =>
+            await _context.Users
+                .AsNoTracking()
+                .Where(u => u.Department == department)
+                .ToListAsync();
+
+        public async Task<List<User>> GetAllAsync() =>
+            await _context.Users
+                .AsNoTracking()
+                .ToListAsync();
     }
 }
