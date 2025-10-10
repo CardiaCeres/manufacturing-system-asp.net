d7c220021eff149d79a1fbbf5777da69497c1889
CardiaCeres <Chilia0324@gmail.com>
2025-10-10 13:47:03 +0800

    Update IOrderRepository.cs

--- manufacturing_system/Repositories/IOrderRepository.cs
+++ manufacturing_system/Repositories/IOrderRepository.cs
@@ -6,10 +6,15 @@ namespace ManufacturingSystem.Repositories
 {
     public interface IOrderRepository
     {
+        // 使用者操作
         Task<List<Order>> GetOrdersByUserIdAsync(long userId);
         Task<Order> AddAsync(Order order);
         Task<Order?> GetByIdAsync(long id);
-        Task DeleteAsync(long id);
         Task<Order> UpdateAsync(Order order);
+        Task DeleteAsync(long id);
+
+        // 管理者操作
+        Task<List<Order>> GetAllAsync(); // 取得所有訂單
+        Task<List<Order>> GetByDepartmentAsync(string department); // 同部門訂單
     }
 }
