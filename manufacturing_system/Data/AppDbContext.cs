b08b6ec11143404730277d502659ce9161649df1
CardiaCeres <Chilia0324@gmail.com>
2025-11-01 14:11:09 +0800

    Update AppDbContext.cs

--- manufacturing_system/Data/AppDbContext.cs
+++ manufacturing_system/Data/AppDbContext.cs
@@ -21,23 +21,6 @@ namespace ManufacturingSystem.Data
 
             base.OnModelCreating(modelBuilder);
 
-            // TokenExpiry 使用 UTC，並指定 PostgreSQL timestamptz
-            var dateTimeConverter = new ValueConverter<DateTime?, DateTime?>(
-                v => v.HasValue ? v.Value.ToUniversalTime() : null,
-                v => v.HasValue ? DateTime.SpecifyKind(v.Value, DateTimeKind.Utc) : null
-            );
-
-            var dateTimeComparer = new ValueComparer<DateTime?>(
-                (c1, c2) => c1.Equals(c2),
-                c => c.HasValue ? c.Value.GetHashCode() : 0,
-                c => c.HasValue ? DateTime.SpecifyKind(c.Value, DateTimeKind.Utc) : null
-            );
-
-            modelBuilder.Entity<User>()
-                .Property(u => u.TokenExpiry)
-                .HasColumnType("timestamptz")
-                .HasConversion(dateTimeConverter)
-                .Metadata.SetValueComparer(dateTimeComparer);
         }
     }
 }
